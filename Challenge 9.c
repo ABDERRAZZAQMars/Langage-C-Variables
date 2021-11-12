30bd9cf3bf8f2a78788b593f8ac3b092352d2253
ABDERRAZZAQ <abderrazzaqmars99@gmail.com>
2021-11-12 12:40:17 +0100

    Distance entre deux points

---
+++ Challenge 9.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float x1 , y1 , x2 , y2 , M , N , MN ;
+
+    printf("Entrer x1 :\t \n");
+    scanf("%f",&x1);
+
+    printf("Entrer y1 :\t \n");
+    scanf("%f",&y1);
+
+    printf("Entrer x2 :\t \n");
+    scanf("%f",&x2);
+
+    printf("Entrer y2 :\t \n");
+    scanf("%f",&y2);
+
+    M = (x2 - x1)*(x2 - x1);
+    N = (y2 - y1)*(y2 - y1);
+
+    MN = sqrt (M+N);
+
+    printf ("la distance entre M et N Est %.1f",MN);
+    
+    return 0;
+}
